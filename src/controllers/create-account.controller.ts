5a12ab1dac0a824eff59d6a6150487ac1d6af980
Gabriel Martins <gab.smartinn@outlook.com>
2026-03-02 15:32:23 -0300

    feat: implement account creation controller with password hashing

---
+++ src/controllers/create-account.controller.ts
@@ -0,0 +1,32 @@
+import { ConflictException } from '@nestjs/common'
+import { Body, Controller, Post } from '@nestjs/common'
+import { hash } from 'bcryptjs'
+import { PrismaService } from 'src/prisma/prisma.service'
+
+@Controller('/accounts')
+export class CreateAccountController {
+  constructor(private readonly prisma: PrismaService) {}
+
+  @Post()
+  async handle(@Body() body: any) {
+    const { name, email, password } = body
+
+    const userWithSameEmail = await this.prisma.user.findUnique({
+      where: { email },
+    })
+
+    if (userWithSameEmail) {
+      throw new ConflictException('User with e-mail already exists.')
+    }
+
+    const hashedPassword = await hash(password, 8)
+
+    await this.prisma.user.create({
+      data: {
+        name,
+        email,
+        password: hashedPassword,
+      },
+    })
+  }
+}
