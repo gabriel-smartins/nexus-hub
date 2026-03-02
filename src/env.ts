99146709d47765ae4aebf8909b5c30f74f82673f
Gabriel Martins <gab.smartinn@outlook.com>
2026-03-02 16:56:41 -0300

    feat: setup jwt authentication module and environment variables

--- src/env.ts
+++ src/env.ts
@@ -6,6 +6,8 @@ export const envSchema = z.object({
   POSTGRES_DB: z.string(),
   DATABASE_URL: z.url(),
   PORT: z.coerce.number().optional().default(3333),
+  JWT_PRIVATE_KEY: z.string(),
+  JWT_PUBLIC_KEY: z.string(),
 })
 
 export type Env = z.infer<typeof envSchema>
