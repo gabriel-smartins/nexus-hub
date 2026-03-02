b0e72f28d05508b98e6a129cc10994d7b9ef29ea
Gabriel Martins <gab.smartinn@outlook.com>
2026-03-02 18:08:06 -0300

    feat: implement fetch recent questions controller with pagination

---
+++ src/controllers/fetch-recent-questions.controller.ts
@@ -0,0 +1,38 @@
+import { Controller, Get, Query } from '@nestjs/common'
+import { ZodValidationPipe } from 'src/pipes/zod-validation-pipe'
+import { PrismaService } from 'src/prisma/prisma.service'
+import z from 'zod'
+
+const pageQueryParamSchema = z
+  .string()
+  .optional()
+  .default('1')
+  .transform(Number)
+  .pipe(z.number().min(1))
+
+type PageQueryParamSchema = z.infer<typeof pageQueryParamSchema>
+
+@Controller('/questions')
+export class FetchRecentQuestionsController {
+  constructor(private readonly prisma: PrismaService) {}
+
+  @Get()
+  async handle(
+    @Query('page', new ZodValidationPipe(pageQueryParamSchema))
+    page: PageQueryParamSchema,
+  ) {
+    const perPage = 20
+
+    const questions = await this.prisma.question.findMany({
+      take: perPage,
+      skip: (page - 1) * perPage,
+      orderBy: {
+        createdAt: 'desc',
+      },
+    })
+
+    return {
+      questions,
+    }
+  }
+}
