ff0a616d750239560245cd77ab8c6d5d47e0015d
Gabriel Martins <gab.smartinn@outlook.com>
2026-03-04 15:04:21 -0300

    refactor: inject register student use case into create account controller

--- src/infra/http/controllers/create-account.controller.ts
+++ src/infra/http/controllers/create-account.controller.ts
@@ -1,9 +1,8 @@
-import { ConflictException, HttpCode, UsePipes } from '@nestjs/common'
+import { HttpCode, UsePipes } from '@nestjs/common'
 import { Body, Controller, Post } from '@nestjs/common'
-import { hash } from 'bcryptjs'
 import { ZodValidationPipe } from '@/infra/http/pipes/zod-validation-pipe'
-import { PrismaService } from '@/infra/database/prisma/prisma.service'
 import { z } from 'zod'
+import { RegisterStudentUseCase } from '@/domain/forum/application/use-cases/register-student'
 
 const createAccountBodySchema = z.object({
   name: z.string(),
@@ -15,12 +14,24 @@ type CreateAccountBodySchema = z.infer<typeof createAccountBodySchema>
 
 @Controller('/accounts')
 export class CreateAccountController {
-  constructor(private readonly prisma: PrismaService) {}
+  constructor(
+    private readonly registerStudentUseCase: RegisterStudentUseCase,
+  ) {}
 
   @Post()
   @HttpCode(201)
   @UsePipes(new ZodValidationPipe(createAccountBodySchema))
   async handle(@Body() body: CreateAccountBodySchema) {
     const { name, email, password } = body
+
+    const result = await this.registerStudentUseCase.execute({
+      name,
+      email,
+      password,
+    })
+
+    if (result.isLeft()) {
+      throw new Error()
+    }
   }
 }
