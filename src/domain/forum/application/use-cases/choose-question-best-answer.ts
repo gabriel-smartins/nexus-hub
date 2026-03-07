cbb91023f086e12b1db11443f0badccfb511c74b
Gabriel Martins <gab.smartinn@outlook.com>
2026-03-07 14:03:10 -0300

    refactor: update choose question best answer use case for dependency injection

--- src/domain/forum/application/use-cases/choose-question-best-answer.ts
+++ src/domain/forum/application/use-cases/choose-question-best-answer.ts
@@ -4,6 +4,7 @@ import { AnswersRepository } from '../repositories/answers-repository'
 import { QuestionRepository } from '../repositories/question-repository'
 import { ResourceNotFoundError } from '@/core/errors/errors/resource-not-found-error'
 import { NotAllowedError } from '@/core/errors/errors/not-allowed-error'
+import { Injectable } from '@nestjs/common'
 
 interface ChooseQuestionBestAnswerUseCaseRequest {
   authorId: string
@@ -17,6 +18,7 @@ type ChooseQuestionBestAnswerUseCaseResponse = Either<
   }
 >
 
+@Injectable()
 export class ChooseQuestionBestAnswerUseCase {
   constructor(
     private questionRepository: QuestionRepository,
@@ -46,6 +48,8 @@ export class ChooseQuestionBestAnswerUseCase {
     }
 
     question.bestAnswerId = answer.id
+
+    await this.questionRepository.save(question)
 
     return right({
       question,
