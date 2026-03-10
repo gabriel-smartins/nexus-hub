e2d7a653762d09935db7a37caa2f7fc2db78e3bd
Gabriel Martins <gab.smartinn@outlook.com>
2026-03-10 14:09:13 -0300

    feat: implement attachment list persistence in memory repositories

--- test/repositories/in-memory-question-attachments-repository.ts
+++ test/repositories/in-memory-question-attachments-repository.ts
@@ -1,10 +1,19 @@
 import { QuestionAttachmentsRepository } from '@/domain/forum/application/repositories/question-attachment-repository'
 import { QuestionAttachment } from '@/domain/forum/enterprise/entities/question-attachment'
 
-export class InMemoryQuestionAttachmentsRepository
-  implements QuestionAttachmentsRepository
-{
+export class InMemoryQuestionAttachmentsRepository implements QuestionAttachmentsRepository {
   public items: QuestionAttachment[] = []
+
+  async createMany(attachments: QuestionAttachment[]) {
+    this.items.push(...attachments)
+  }
+  async deleteMany(attachments: QuestionAttachment[]) {
+    const questionAttachments = this.items.filter((item) => {
+      return !attachments.some((attachment) => attachment.equals(item))
+    })
+
+    this.items = questionAttachments
+  }
 
   async findManyByQuestionId(questionId: string) {
     const questionAttachments = this.items.filter(
