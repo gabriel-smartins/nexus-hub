4b6cfcfa863eda6e5c1ca764298680adbfe024a3
Gabriel Martins <gab.smartinn@outlook.com>
2026-03-11 12:06:36 -0300

    feat: create question details value object and update repository contract

--- src/domain/forum/application/repositories/question-repository.ts
+++ src/domain/forum/application/repositories/question-repository.ts
@@ -1,5 +1,6 @@
 import { PaginationParams } from '@/core/repositories/pagination-params'
 import { Question } from '../../enterprise/entities/question'
+import { QuestionDetails } from '../../enterprise/entities/value-objects/question-details'
 
 export abstract class QuestionRepository {
   abstract create(question: Question): Promise<void>
@@ -7,5 +8,6 @@ export abstract class QuestionRepository {
   abstract findById(id: string): Promise<Question | null>
   abstract findManyRecent(params: PaginationParams): Promise<Question[]>
   abstract findBySlug(slug: string): Promise<Question | null>
+  abstract findDetailsBySlug(slug: string): Promise<QuestionDetails | null>
   abstract delete(question: Question): Promise<void>
 }
